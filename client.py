e2b70c9d10cb2c5c11a9b37fc05bb8a4dd27ce66
AKASH1925 <takash2412@gmail.com>
2026-02-09 10:52:44 +0530

    Fix capitraliziatom

--- client.py
+++ client.py
@@ -4,5 +4,6 @@ socket.SOCK_STREAM)
 client_socket.connect(("127.0.0.1", 12345)) # Connect to the server
 client_socket.send(b"Hello from client") 
 response = client_socket.recv(1024) 
-print(f"Received from server: {response.decode()}") 
+print(f"Received from Server: {response.decode()}") 
 client_socket.close()
+
